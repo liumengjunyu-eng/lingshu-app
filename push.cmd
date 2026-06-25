729f2d921c85c37502535df42aa931cfb6b8e101
liumengjunyu-eng <liumengjunyu@gmail.com>
2026-06-26 05:27:21 +0800

    chore: remove temp push scripts

--- push.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "D:\Projects\lingshu-app"
-git add -A
-git commit -m "feat: report page + bazi-engine + wellness-data fully translated to English"
-git push origin main
--- push.ps1
+++
@@ -1,4 +0,0 @@
-Set-Location -LiteralPath 'D:\Projects\lingshu-app'
-git add -A
-git commit -m "feat: report page + bazi-engine + wellness-data fully translated to English"
-git push origin main
