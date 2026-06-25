f7b463735672ef16f6c422ca86ff9e5f242a80c9
liumengjunyu-eng <liumengjunyu@gmail.com>
2026-06-25 16:34:29 +0800

    chore: add lucide-react icon library

--- do-install.js
+++ do-install.js
@@ -1,14 +1,5 @@
 const { execSync } = require('child_process');
-const projectDir = 'C:\\Users\\刘\\Desktop\\lingshu-app';
-console.log('Working directory:', projectDir);
-try {
-  const result = execSync('npm install lunar-javascript', {
-    cwd: projectDir,
-    encoding: 'utf8'
-  });
-  console.log(String(result));
-  console.log('SUCCESS');
-} catch(e) {
-  console.error('FAILED:', String(e.stderr || e.message));
-  process.exit(1);
-}
+execSync('npm install lucide-react', {
+  cwd: __dirname,
+  stdio: 'inherit'
+});
---
+++ install-lucide.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d D:\Projects\lingshu-app
+call npm install lucide-react
