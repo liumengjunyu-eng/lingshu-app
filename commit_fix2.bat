695ad1a5ac19cba53e76d1ac2445a4e545c9a8d9
liumengjunyu-eng <liumengjunyu@gmail.com>
2026-06-26 07:14:16 +0800

    chore: remove helper scripts

--- commit_fix2.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "D:\Projects\lingshu-app"
-git add app/deep-report/page.tsx
-git status
-git commit -m "fix: use BaziResult field names correctly in deep-report"
-git log --oneline -3
--- push_fix2.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "D:\Projects\lingshu-app"
-git add app/deep-report/page.tsx
-git commit -m "fix: use BaziResult field names correctly in deep-report"
-git push
-echo Done!
